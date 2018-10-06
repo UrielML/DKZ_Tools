afb1a7adbd02fc77a5359628196148341ff3598b
Uriel Mgallon Lugo <1715110302@utec-tgo.edu.mx>
2018-10-06 14:34:08 -0500

    caesar developed

--- app/src/main/java/net/bitgeekenviroment/dkz_tools/Caesar_Cipher.java
+++ app/src/main/java/net/bitgeekenviroment/dkz_tools/Caesar_Cipher.java
@@ -2,16 +2,27 @@ package net.bitgeekenviroment.dkz_tools;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class Caesar_Cipher extends AppCompatActivity {
-    EditText keytxt, encode, decode;
-    private String values = "abcdefghijklmnopqrstuvwxyz0123456789";
+public class Caesar_Cipher extends AppCompatActivity implements View.OnClickListener {
+    EditText keytxt, encode, decode, values;
+    Button toEncode, toDecode;
+    //private String values = "abcdefghijklmnopqrstuvwxyz0123456789";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caesar__cipher);
+        keytxt=findViewById(R.id.et_key);
+        encode=findViewById(R.id.et_code);
+        decode=findViewById(R.id.et_decode);
+        values=findViewById(R.id.et_values);
+        toEncode=findViewById(R.id.btn_encode);
+        toDecode=findViewById(R.id.btn_decode);
+        toEncode.setOnClickListener(this);
+        toDecode.setOnClickListener(this);
     }
 
 
@@ -27,7 +38,7 @@ public class Caesar_Cipher extends AppCompatActivity {
     public void decode(){
         try {
             Integer.parseInt(keytxt.getText().toString());
-            encode.setText(Decode(decode.getText().toString(), Integer.parseInt(keytxt.getText().toString())));
+            decode.setText(Decode(encode.getText().toString(), Integer.parseInt(keytxt.getText().toString())));
         } catch(NumberFormatException e1) {
             //JOptionPane.showMessageDialog(this, "Enter digits only");
         }
@@ -41,9 +52,10 @@ public class Caesar_Cipher extends AppCompatActivity {
         text = text.toLowerCase();
         for (int i = 0; i < text.length(); i++) {
             charEnc = text.charAt(i);
-            valEnc = values.indexOf(charEnc);
-            newEnc = (key + valEnc) % values.length();
-            encrypted += values.charAt(newEnc);
+            String valu=values.getText().toString();
+            valEnc = valu.indexOf(charEnc);
+            newEnc = (key + valEnc) % valu.length();
+            encrypted += valu.charAt(newEnc);
         }
         return encrypted.toUpperCase();
     }
@@ -53,17 +65,28 @@ public class Caesar_Cipher extends AppCompatActivity {
         int valEnc;
         int newEnc;
         String decoded = "";
+        String valu=values.getText().toString();
         text = text.toLowerCase();
         for (int i = 0; i < text.length(); i++) {
             charEnc = text.charAt(i);
-            valEnc = values.indexOf(charEnc);
-            newEnc = (valEnc - key) % values.length();
+            valEnc = valu.indexOf(charEnc);
+            newEnc = (valEnc - key) % valu.length();
             // check for negative
             if (newEnc < 0) {
-                newEnc += Math.abs(values.length());
+                newEnc += Math.abs(valu.length());
             }
-            decoded += values.charAt(newEnc);
+            decoded += valu.charAt(newEnc);
         }
         return decoded.toUpperCase();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==toDecode){
+            decode();
+        }
+        if(v==toEncode){
+            encode();
+        }
     }
 }
--- app/src/main/java/net/bitgeekenviroment/dkz_tools/MainMenu.java
+++ app/src/main/java/net/bitgeekenviroment/dkz_tools/MainMenu.java
@@ -1,13 +1,26 @@
 package net.bitgeekenviroment.dkz_tools;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
-
+public class MainMenu extends AppCompatActivity implements View.OnClickListener {
+    Button caesar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        caesar=findViewById(R.id.btn_CaesarCipher);
+        caesar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==caesar){
+            Intent i = new Intent(this, Caesar_Cipher.class);
+            startActivity(i);
+        }
     }
 }
